de9f875d114b2ceb4222647c6d05de3ffea671d3
FedorIzvekov <fedor.izvekov@gmail.com>
2016-08-13 17:27:19 +0300

    websocket-server-servlet: added websocket servlet

--- websocket-server-servlet/src/main/java/com/fedorizvekov/websocket/server/servlet/WebsocketServerServlet.java
+++ websocket-server-servlet/src/main/java/com/fedorizvekov/websocket/server/servlet/WebsocketServerServlet.java
@@ -2,8 +2,10 @@ package com.fedorizvekov.websocket.server.servlet;
 
 import java.io.InputStream;
 import java.util.Properties;
+import com.fedorizvekov.websocket.server.servlet.servlet.WebsocketMessengerServlet;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 
 public class WebsocketServerServlet {
 
@@ -18,6 +20,7 @@ public class WebsocketServerServlet {
         int port = Integer.parseInt(properties.getProperty("server.port"));
 
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new WebsocketMessengerServlet()), "/messenger");
 
         Server server = new Server(port);
         server.setHandler(context);
